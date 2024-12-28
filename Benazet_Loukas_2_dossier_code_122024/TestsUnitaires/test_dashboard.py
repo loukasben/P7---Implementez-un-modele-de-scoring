0541d47b5b38b16b42c96f021a332fd90c0677e2
loukasben <141060799+loukasben@users.noreply.github.com>
2024-12-28 16:21:48 +0100

    Update test_dashboard.py

--- Benazet_Loukas_2_dossier_code_122024/TestsUnitaires/test_dashboard.py
+++ Benazet_Loukas_2_dossier_code_122024/TestsUnitaires/test_dashboard.py
@@ -18,7 +18,7 @@ mocked_session_state = {'state': {'data_received': False, 'data': None, 'last_sk
 # Définir le décorateur pytest pour simuler st.session_state
 @pytest.fixture
 def mocked_st(monkeypatch):
-    # Utilisez monkeypatch pour remplacer l'attribut session_state de streamlit par notre état simulé
+    # Utiliser monkeypatch pour remplacer l'attribut session_state de streamlit par notre état simulé
     monkeypatch.setattr("streamlit.session_state", mocked_session_state)
     return mocked_session_state
 
@@ -27,7 +27,7 @@ def mocked_st(monkeypatch):
 # Utiliser pytest.mark pour appliquer le mock à des tests spécifiques
 @pytest.mark.parametrize('mocked_st', [mocked_session_state], indirect=True)
 def test_compute_color(mocked_st):
-    # Importez la fonction compute_color à partir du module dashboard
+    # Importer la fonction compute_color à partir du module dashboard
     from dashboard import compute_color
     # Teste la fonction compute_color pour différentes valeurs
     assert compute_color(30) == "green", "Erreur dans la fonction compute_color."
@@ -35,7 +35,7 @@ def test_compute_color(mocked_st):
 
 @pytest.mark.parametrize('mocked_st', [mocked_session_state], indirect=True)
 def test_format_value(mocked_st):
-    # Importez la fonction format_value à partir du module dashboard
+    # Importer la fonction format_value à partir du module dashboard
     from dashboard import format_value
     # Teste la fonction format_value pour différentes valeurs
     assert format_value(5.67) == 5.67, "Erreur dans la fonction format_value."
@@ -43,14 +43,14 @@ def test_format_value(mocked_st):
 
 @pytest.mark.parametrize('mocked_st', [mocked_session_state], indirect=True)
 def test_find_closest_description(mocked_st):
-    # Importez les fonctions find_closest_description et definition_features_df à partir du module dashboard
+    # Importer les fonctions find_closest_description et definition_features_df à partir du module dashboard
     from dashboard import find_closest_description, definition_features_df
     # Teste la fonction pour trouver la description la plus proche d'un terme donné
     description = find_closest_description("AMT_INCOME_TOTAL", definition_features_df)
     assert description is not None, "Erreur dans la fonction find_closest_description."
 
 def test_get_state(mocked_st):	
-    # Importez la fonction get_state à partir du module dashboard
+    # Importer la fonction get_state à partir du module dashboard
     from dashboard import get_state
     state = get_state()
     assert isinstance(state, dict), "La fonction get_state doit renvoyer un dictionnaire."
