b5dca85c2c3037c5831d5bbd93998ed8163891c3
loukasben <141060799+loukasben@users.noreply.github.com>
2024-12-28 17:32:24 +0100

    Update test_api.py

--- Benazet_Loukas_2_dossier_code_122024/TestsUnitaires/test_api.py
+++ Benazet_Loukas_2_dossier_code_122024/TestsUnitaires/test_api.py
@@ -6,7 +6,7 @@ import pytest
 from flask import Flask, jsonify, request
 
 # Ajouter le chemin relatif du fichier api.py au sys.path pour pouvoir l'importer
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Scripts')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'APIDashboard')))
 
 # Importer les éléments nécessaires du fichier api.py
 from api import app, current_directory, model, predict
@@ -21,7 +21,7 @@ def client():
 # Tester le chargement du modèle de prédiction
 def test_model_loading():
     # Détermine le chemin du fichier contenant le modèle entraîné
-    model_path = os.path.join(current_directory, "..", "Simulations", "Best_model", "model.pkl")
+    model_path = os.path.join(current_directory, "..", "Prediction", "model.pkl")
     # Charge le modèle à partir du fichier
     model = joblib.load(model_path)
     # Vérifie que le modèle a été chargé correctement
@@ -30,7 +30,7 @@ def test_model_loading():
 # Tester le chargement du fichier CSV contenant les données de train
 def test_csv_loading():
     # Détermine le chemin du fichier CSV
-    csv_path = os.path.join(current_directory, "..", "Simulations", "Data", "df_train.csv")
+    csv_path = os.path.join(current_directory, "..", "Prediction", "df_train.csv")
     # Charge le fichier CSV dans un DataFrame pandas
     df = pd.read_csv(csv_path)
     # Vérifie que le DataFrame n'est pas vide
@@ -44,7 +44,7 @@ def test_prediction():
     # Détermine le chemin du répertoire courant
     current_directory = os.path.dirname(os.path.abspath(__file__))
     # Détermine le chemin du fichier CSV contenant les données de test
-    csv_path = os.path.join(current_directory, "..", "Simulations", "Data", "df_train.csv")
+    csv_path = os.path.join(current_directory, "..", "Prediction", "df_train.csv")
     # Charge le fichier CSV dans un DataFrame pandas
     df = pd.read_csv(csv_path)
     # Prend un échantillon pour la prédiction
